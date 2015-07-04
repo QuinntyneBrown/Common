7dc8bc02e3be037d368ba590df00ca9b6998eaf9
Quinntyne Brown <qbrown@cetaris.com>
2015-07-04 13:43:37 -0400

    initial commit

---
+++ Common/Config/CommonConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Config
+{
+    public class CommonConfiguration : ConfigurationSection
+    {
+        [ConfigurationProperty("internalSecret")]
+        public string InternalSecret
+        {
+            get { return (string)this["internalSecret"]; }
+            set { this["internalSecret"] = value; }
+        }
+
+        public static CommonConfiguration Config
+        {
+            get { return ConfigurationManager.GetSection("commonConfiguration") as CommonConfiguration; }
+        }
+    }
+}
